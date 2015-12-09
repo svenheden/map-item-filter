c1f0fe463296405dec28cf250e59303948f8afe5
Jonathan Svenheden <jonathan@svenheden.com>
2015-12-09 09:17:08 -0500

    Refactor render function of `FilterSummary` into 3 smaller functions

--- scripts/components/filter-summary.js
+++ scripts/components/filter-summary.js
@@ -1,35 +1,51 @@
 import React, { PropTypes } from 'react'; // eslint-disable-line no-unused-vars
 import Pill from '../components/pill';
 
-const FilterSummary = (props) => {
-  const matches = props.numberOfItems + (props.numberOfItems === 1 ? ' träff' : ' träffar');
-  const hasCategory = props.category.id !== 0;
-  const hasSubCategories = props.subCategories.length > 0;
+const FilterSummary = (props) => (
+  <div className="filter-summary">
+    {renderNumberOfMatches(props)}
+    {renderSubCategories(props)}
+    {renderCategory(props)}
+  </div>
+);
 
-  return (
-    <div className="filter-summary">
-      Visar {matches} {props.googleMapsIsLoaded ? 'inom det aktuella området på ' : 'på '}
-      {props.subCategories.map(subCategory =>
-        <Pill
-          key={subCategory.id}
-          id={subCategory.id}
-          label={subCategory.label}
-          tooltip={`Ta bort ${subCategory.label} från filtreringen`}
-          onClick={() => props.onClickSubCategory(subCategory.id)}
-        />
-      )}
-      {hasSubCategories && ' inom ' }
-      {hasCategory &&
-        <Pill
-          id={props.category.id}
-          label={props.category.label}
-          tooltip={`Ta bort ${props.category.label} från filtreringen`}
-          onClick={() => props.onClickCategory()}
-        />
-      }
-      {!hasCategory && ' alla enheter'}
-    </div>
-  );
+const renderNumberOfMatches = (props) => (
+  'Visar ' + props.numberOfItems +
+    (props.numberOfItems === 1
+      ? ' träff'
+      : ' träffar' ) +
+    (props.googleMapsIsLoaded
+      ? ' inom det aktuella området på '
+      : ' på ')
+);
+
+const renderSubCategories = (props) => {
+  if (props.subCategories.length > 0) {
+    return props.subCategories.map(subCategory =>
+      <Pill
+        key={subCategory.id}
+        id={subCategory.id}
+        label={subCategory.label}
+        tooltip={`Ta bort ${subCategory.label} från filtreringen`}
+        onClick={() => props.onClickSubCategory(subCategory.id)}
+      />
+    ).concat(' inom ');
+  }
+};
+
+const renderCategory = (props) => {
+  if (props.category.id !== 0) {
+    return (
+      <Pill
+        id={props.category.id}
+        label={props.category.label}
+        tooltip={`Ta bort ${props.category.label} från filtreringen`}
+        onClick={() => props.onClickCategory()}
+      />
+    );
+  } else {
+    return ' alla enheter';
+  }
 };
 
 const categoryShape = PropTypes.shape({
